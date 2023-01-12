15d43924d27377ba79ecb82786da3febef67c530
TelamarAndrew <andrew.scherbyna@sigma.software>
2023-01-12 20:53:41 +0200

    Added practise for StorageQueue

---
+++ src/StorageQueue/README.md
@@ -0,0 +1,11 @@
+# Azure Commands
+
+myLocation=northeurope
+myResourceGroup=az204-strgqueue-rg
+myStorageAccount=az204strgqueueacc
+
+az group create --name $myResourceGroup --location $myLocation
+
+az storage account create --name $myStorageAccount  --resource-group $myResourceGroup
+
+az group delete --name $myResourceGroup --no-wait
---
+++ src/StorageQueue/StorageQueue.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+string queueName = "mycustomqueue";
+string connectionString = "<Connection String Storage Account>";
+
+var queueClient = new QueueClient(connectionString, queueName);
+
+await queueClient.CreateIfNotExistsAsync();
+
+Response<SendReceipt> sentMessageResponse = await queueClient.SendMessageAsync("Hello World!");
+
+Console.WriteLine($"Message has been published. The message ID is {sentMessageResponse.Value.MessageId}");
+
+Response<PeekedMessage> peekedMessageResponse = await queueClient.PeekMessageAsync();
+
+Console.WriteLine($"Message has been peeked. The message is {peekedMessageResponse.Value.Body.ToString()}");
+
+Response<QueueMessage> lockedMessageResponse = await queueClient.ReceiveMessageAsync();
+
+Console.WriteLine($"Message has been received and locked. The message is {lockedMessageResponse.Value.Body.ToString()}");
+
+Response<UpdateReceipt> updatedMessageResponse = await queueClient.UpdateMessageAsync(
+	lockedMessageResponse.Value.MessageId,
+	lockedMessageResponse.Value.PopReceipt,
+	"Updated Hello World",
+	TimeSpan.FromSeconds(5));
+
+Console.WriteLine($"Message has been updated. The message will be visible on {updatedMessageResponse.Value.NextVisibleOn}");
+Console.WriteLine("Sleep for 5 seconds");
+
+await Task.Delay(TimeSpan.FromSeconds(5));
+
+Response<QueueMessage> newLockedMessageResponse = await queueClient.ReceiveMessageAsync();
+
+Console.WriteLine($"Message has been received and locked. The message is {newLockedMessageResponse.Value.Body.ToString()}");
+
+Response deletedResponse = await queueClient.DeleteMessageAsync(
+	newLockedMessageResponse.Value.MessageId,
+	newLockedMessageResponse.Value.PopReceipt);
+
+Console.WriteLine($"Message has been deleted");
+
+await queueClient.DeleteIfExistsAsync();
+
+Console.WriteLine($"Queue has been deleted");
+Console.ReadKey();
