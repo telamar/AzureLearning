0625952fca8994730d53a2af9461b941132773af
TelamarAndrew <andrew.scherbyna@sigma.software>
2023-01-13 18:10:44 +0200

    Added Functions practice

---
+++ src/Functions/HttpExampleFunction.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace Functions
+{
+    public static class HttpExampleFunction
+    {
+        [FunctionName("HttpExampleFunction")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+        {
+			string htmlResponse = ConvertQueryToHtmlResponse(req);
+
+			return new ContentResult()
+			{
+				Content = htmlResponse,
+				ContentType = "text/html",
+				StatusCode = 200
+			};
+        }
+
+        private static string ConvertQueryToHtmlResponse(HttpRequest req)
+        {
+			var stringBuilder = new StringBuilder();
+
+			stringBuilder.AppendLine("<h1>Your request contains the following params:</h1>");
+			stringBuilder.AppendLine("<ul>");
+
+			foreach (var queryParam in req.Query)
+			{
+				stringBuilder.AppendLine($"<li>Param: {queryParam.Key}, value: {queryParam.Value}</li>");
+			}
+
+			stringBuilder.AppendLine("</ul>");
+
+            return stringBuilder.ToString();
+		}
+	}
+}
---
+++ src/Functions/QueueExampleFunction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+
+namespace Functions
+{
+	public class QueueExampleFunction
+	{
+		[FunctionName("QueueExampleFunction")]
+		public static void Run(
+			[QueueTrigger("myqueue-items-source", Connection = "AzureWebJobsStorage")] string myQueueItem,
+			[Queue("myqueue-items-target", Connection = "AzureWebJobsStorage")] out string myQueueItemCopy)
+		{
+			myQueueItemCopy = myQueueItem;
+		}
+	}
+}
